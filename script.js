04e6420381e325333b0dc6f3d38050bde3a0bb50
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-10-01 20:16:41 +0530

    Updated Stopwatch function for leading Zeroes.

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
+// Variables for Leading Zeroes
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
 // Stop Watch Function
 
 function stopWatch() {
@@ -24,8 +30,26 @@ function stopWatch() {
     }
   }
 
+  if (seconds < 10) {
+    leadingSeconds = "0" + seconds.toString();
+  } else {
+    leadingSeconds = seconds;
+  }
+
+  if (minutes < 10) {
+    leadingMinutes = "0" + minutes.toString();
+  } else {
+    leadingMinutes = minutes;
+  }
+
+  if (hours < 10) {
+    leadingHours = "0" + hours.toString();
+  } else {
+    leadingHours = hours;
+  }
+
   let displayTimer = (document.getElementById("timer").innerText =
-    hours + ":" + minutes + ":" + seconds);
+    leadingHours + ":" + leadingMinutes + ":" + leadingSeconds);
 }
 
 // window.setInterval(stopWatch, 1000);
